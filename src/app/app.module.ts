9ec5a8b9f718e34d58ec4b44745726da96269b64
masashiGMS <masashikatsumata.gms@gmail.com>
2017-10-27 19:47:42 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GoogleMaps } from "@ionic-native/google-maps";
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {
+  NavController,
+  Platform,
+  ToastController
+} from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapsAnimation,
+  Marker
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +18,92 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mapReady: boolean = false;
+  map: GoogleMap;
 
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    public toastCtrl: ToastController,
+    private googleMaps: GoogleMaps) {
   }
 
+  ionViewDidLoad() {
+    setTimeout(this.loadMap.bind(this), 1000);
+  }
+
+  loadMap() {
+    // Create a map after the view is ready and the native platform is ready.
+    this.map = this.googleMaps.create('map_canvas');
+
+    // Wait the maps plugin is ready until the MAP_READY event
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      this.showToast('map is ready to use.');
+      this.mapReady = true;
+
+      
+    });
+  }
+
+  createMarkers() {
+    var latLngBounds = this.map.getVisibleRegion();
+    var sw = latLngBounds.southwest;
+    var ne = latLngBounds.northeast;
+    var diffY = (ne.lat - sw.lat);
+    var diffX = (ne.lng - sw.lng);
+    for (var i = 0; i < 100; i++) {
+      this.map.addMarker({
+        'position': {
+          'lat': sw.lat + diffY * Math.random(),
+          'lng': sw.lng  + diffX * Math.random()
+        }
+      });
+    }
+
+  }
+  onButtonClick() {
+    if (!this.mapReady) {
+      this.showToast('map is not ready yet. Please try again.');
+      return;
+    }
+
+    // Get the location of you
+    this.map.getMyLocation().then((location) => {
+      console.log(JSON.stringify(location, null ,2));
+
+      // Move the map camera to the location with animation
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        tilt: 30
+      }).then(() => {
+
+        // add a marker
+        this.map.addMarker({
+          title: '@ionic-native/google-maps plugin!',
+          snippet: 'This plugin is awesome!',
+          position: location.latLng,
+          animation: GoogleMapsAnimation.BOUNCE
+        }).then((marker: Marker) => {
+          // show the infoWindow
+          marker.showInfoWindow();
+
+          // If clicked it, display the alert
+          marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+            this.showToast('clicked!');
+          });
+        });
+      });
+    });
+  }
+
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present(toast);
+  }
 }
